442c49120512bcf6c8a23ee7420c384681d41d6b
Joe Drago <joedrago@gmail.com>
2023-12-16 02:54:52 +0000

    fix for skittles

--- gif.js
+++ gif.js
@@ -1,21 +1,26 @@
+#!/usr/bin/env node
+
 const fs = require("fs")
 const temp = require("temp")
 const { spawnSync } = require("child_process")
 
 const main = (argv) => {
     const url = argv.shift()
+    const outputFilename = argv.shift()
     const start = argv.shift()
     const duration = argv.shift()
-    if (!url || !start || !duration) {
-        console.error("Syntax: gif.js [url] [startSeconds] [durationSeconds]")
+    if (!url || !outputFilename || !start || !duration) {
+        console.error("Syntax: gif.js [url] [outputFilename] [startSeconds] [durationSeconds]")
         process.exit(1)
     }
 
     const tempVideoFilename = temp.path({ suffix: ".mp4" })
-    const tempGifFilename = temp.path({ suffix: ".gif" })
 
-    console.log(`TempVid: ${tempVideoFilename}`)
-    console.log(`TempGif: ${tempGifFilename}`)
+    console.log(`URL     : ${url}`)
+    console.log(`Output  : ${outputFilename}`)
+    console.log(`Start   : ${start}`)
+    console.log(`Duration: ${duration}`)
+    console.log(`Temp    : ${tempVideoFilename}`)
 
     spawnSync("yt-dlp", ["--remux-video", "mp4", "-f", "bv", "-o", tempVideoFilename, url], { stdio: "inherit" })
     if (!fs.existsSync(tempVideoFilename)) {
@@ -34,7 +39,7 @@ const main = (argv) => {
             tempVideoFilename,
             "-filter_complex",
             "[0:v] fps=12,scale=480:-1,split [a][b];[a] palettegen [p];[b][p] paletteuse",
-            tempGifFilename
+            outputFilename
         ],
         { stdio: "inherit" }
     )
